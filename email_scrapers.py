f32d372d97eba06df3a7ec5a1e203e99b9074ce2
thanujakmt <thanujakmsdr@gmail.com>
2023-04-25 17:05:28 +0530

    selenium added

--- email_scrapers.py
+++ email_scrapers.py
@@ -1,12 +1,32 @@
-
+# ============== Packages =================
 
 from loopers import loop
 from verify_email import verify_email
 import re
 import requests
+from driver import Driver
+
+# =============== declaring functions =================
+# =============== taking url main domain ==============
 
 def get_email_from_loopers(url):
-    return list(set(list(loop(url).values())[0]['emails']))
+
+    loopers_dict_value = list(loop(url).values())
+    
+
+    if loopers_dict_value:
+       
+        emails_list  = loopers_dict_value[0]['emails']
+       
+    else:
+        emails_list = None
+
+    if emails_list is None:
+        return None
+    else:
+        return list(set(emails_list))
+    
+# =============== verifing emails ======================
 
 def get_valided_emails(emails):
     verify_emails_list = []
@@ -15,20 +35,24 @@ def get_valided_emails(emails):
             verify_emails_list.append(email)
     print(verify_emails_list)
 
+# ================ checking url status ==================
+
 def get_status_of_url(contact_url):
-    return requests.get(contact_url,timeout=5,verify=False).status_code
+    try:
+        statusCode =  requests.get(contact_url,timeout=5,verify=False).status_code
+    except Exception as e:
+        statusCode = 404
+    return statusCode
 
 def get_contact_urls(url):
 
     contact_sub_domain_list = [
-        'contact','contactus','contacts','contact-us','contact-us.php','contactus.php','contact.php','contactus.html','about-us','aboutus'
+       'contact-us-and-hours','contact','contactus','contacts','contact-us','contact-us.php','contactus.php','contact.php','contactus.html','about-us','aboutus','contact.html'
     ]
 
     contact_sub_domain_url = []
     regex = r"^(?:https?:\/\/)?(?:[^@\/\n]+@)?(?:www\.)?([^:\/\n]+)"
     regex_home_url_object = re.finditer(regex,url)
-
-    
 
     for url in regex_home_url_object:
         home_url = (str(url).split("='")[-1].replace("'>",""))
@@ -45,12 +69,74 @@ def get_contact_urls(url):
     
     return contact_sub_domain_url
 
-if __name__=='__main__':
+# =================== declaration of main function ===================
+def get_page_source(url):
+
+    driver = Driver()
+    driver.get(url)
+    page_source_txt = str(driver.page_source)
+    driver.close()
+    return page_source_txt
+
+def get_email_from_selenium_webdriver(url):
+    
+    text = get_page_source(url)
+    # print(text)
+
+    email_regex = re.compile(r'''(
+                [a-zA-Z0-9._%+-]+
+                @
+                [a-zA-Z0-9.-]+
+                (\.[a-zA-Z]{2,4})
+                )''', re.VERBOSE)       
+    try:
+        matches = set([groups[0] for groups in email_regex.findall(text)])
+        return matches if matches else None
+    except Exception as err:
+        print(" {} \nFailed to read page!".format(err))
+        return None
+
+def Email_Scrapper():
     url='https://www.vmokshagroup.com/'
+    # url = "https://usbiomag.com/"
+    # url = "https://www.ancientrootsacu.com/" #selenium
+    # url = "https://www.zentralabq.com" #***selenium
+    # url = "https://www.onpoint-acupuncture.com/" #***selenium
+
+    # url = "https://www.rover.com/" #***
+    # url = "https://gpcacupuncture.com/"
+    # url = "https://www.jadestaracupuncture.com/" #*** 
+    # url = "http://www.tucsonacupuncture.com/" #selenu
+
+    email = get_email_from_loopers(url)
+   
+    if email:
+        
+        return email
+    
+    if not email:
+        email = get_email_from_selenium_webdriver(url)
+        if email:
+            return list(set(email))
+        
+    if not email:
+        contact_url = get_contact_urls(url)
+        for url in contact_url:
+            email = get_email_from_loopers(url)
+            if email:
+                return email
+            else:
+                email = get_email_from_selenium_webdriver(url)
+                if email:
+                    return list(set(email))
+                
+if __name__=='__main__':
+    emails = Email_Scrapper()
+    print(emails)
     # emails = get_email_from_loopers(url)
     # print(emails)
     # get_valided_emails(emails)
 
     # get_contact_urls(url)
     # print(get_status_of_url(url))
-    print(get_contact_urls(url))
+    # print(get_contact_urls(url))
